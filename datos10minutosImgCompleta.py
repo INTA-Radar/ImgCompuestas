3a5b2fa1f99dc11cd655805b0d075453fe6564f9
Yanina Bellini Saibene <yabellini@gmail.com>
2015-01-09 16:44:07 -0300

    Licencia
    Agregado de licencia al script

--- datos10minutosImgCompleta.py
+++ datos10minutosImgCompleta.py
@@ -1,5 +1,29 @@
-# -*- encoding: utf8
-# Autores: Bellini Saibene Yanina;Banchero, Santiago
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       datos10minutosImgCompleta Versión 1.0 - Enero 2015
+#       
+#       Autores: Bellini Saibene Yanina;Banchero, Santiago
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#       
+#       
+#		Este script es parte del set de software desarrollado por INTA
+#		para el tratamiento de los datos de los radares meteorológicos.
+
 
 import argparse
 import pyodbc
